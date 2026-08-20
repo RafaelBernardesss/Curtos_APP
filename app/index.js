ad31c9c8b674d6ef0f6357fccc398f7c645a336f
R1R2R3R4@r <rajingg7@gmail.com>
2026-08-20 13:08:10 -0300

    Arrumando o erro

---
+++ app/index.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import React from "react";
+import { 
+    View,
+    Text,
+    StyleSheet,
+    ScrollView,
+    SafeAreaViewBase,
+    TouchableOpacity
+ } from "react-native";
+
+ const [name, setName] = useState('');
+ const [senha, setSenha] = useState('');
+ const [email, setEmail] = useState('');
+
+export default function App() {
+    return(
+        <View></View>
+    );
+}
