e8dfc7f53cbed305c02c514f45e44db40d13f97d
R1R2R3R4@r <rajingg7@gmail.com>
2026-08-24 12:50:09 -0300

    Terminando as telas de login

--- app/RegisterScreen.js
+++ app/RegisterScreen.js
@@ -1,11 +1,71 @@
 import React from "react";
+import { useState } from "react";
 import {
     View,
-    Text
+    Text,
+    StyleSheet,
+    KeyboardAvoidingView,
+    ScrollView,
+    Platform,
+    TouchableOpacity
 } from 'react-native';
+import AppInput from '../src/components/AppInput';
+import AppButton from '../src/components/AppButton';
+import { COLORS } from '../src/constants/Theme';
+import {useRouter} from 'expo-router'
 
-export default function Register(){
-    return(
-        <Text>Você esta na pagina de registro</Text>
+export default function Register() {
+
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const router = useRouter();
+
+    return (
+            <KeyboardAvoidingView style={styles.container} behavior={Platform.OS==='ios' ? 'padding': 'height'}>
+                <View style={styles.container}>
+                    <Text style={styles.title}>Registre-se</Text>
+                    <Text style={styles.subtitle}>Crie uma conta</Text>
+                    <AppInput label="Nome" placeholder="Digite seu nome" value={nome} onChangeText={setNome}/>
+                    <AppInput label="Email" placeholder="exemple@gmail.com" autoCapitalize="none" keyboardType="email-address"
+                    value={email} onChangeText={setEmail} />
+                    <AppInput label="Senha" placeholder="Digite sua senha" secureTextEntry value={senha} onChangeText={setSenha} />
+                    <AppInput label="Confirmar senha" placeholder="Confirme sua senha" secureTextEntry value={senha}
+                    onChangeText={setSenha} />
+                    <AppButton title="Registrar" loading={loading}/>
+                    <TouchableOpacity>
+                        <Text style={styles.link} onPress={() => router.push('/index')}>Já tem uma conta ?</Text>
+                    </TouchableOpacity>
+                </View>
+            </KeyboardAvoidingView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingHorizontal: 24,
+        paddingBottom: '20%'
+    },
+    title:{
+        fontSize: 34,
+        fontWeight: '900',
+        color: COLORS.primary,
+        textAlign: 'center'
+    },
+    subtitle:{
+        color: '#7f8c8d',
+        textAlign: 'center',
+        marginTop: 20,
+        fontWeight: '700'
+    },
+    link:{
+        color:  COLORS.primaryDark,
+        textAlign: 'center',
+        marginTop: 20,
+        fontWeight: '700'
+    },
+});
